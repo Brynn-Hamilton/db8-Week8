a5622795cbb694236d59fb4cdc21cd72f4161b1a
Brynn Hamilton <brynnhamilton1@rocketmortgage.com>
2022-09-20 15:06:51 -0400

    Uploading lab 8.2 coffee shop registration

---
+++ Lab8.2-Coffee Shop Registration/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Lab8._2_Coffee_Shop_Registration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lab8._2_Coffee_Shop_Registration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult RegistrationLogin()
+        {
+            return View();
+        }
+
+        public IActionResult RegistrationSuccess(string firstName, string lastName, string email, string password)
+        {
+            ViewBag.FirstName = firstName;
+            ViewBag.LastName = lastName;
+            ViewBag.Email = email;
+            ViewBag.Password = password;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
